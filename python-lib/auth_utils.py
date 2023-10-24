ee1dc485cb9069c571377ce6b02f5ecd831c19d4
chrisdesilvakoverse <chrisdesilva@koverse>
2023-10-24 15:08:03 -0700

    made initial changes for keycloak auth

--- python-lib/auth_utils.py
+++ python-lib/auth_utils.py
@@ -18,6 +18,30 @@ def get_jwt(preset, kdp_conn, logger):
         jwt = authentication_details.get("access_token")
 
         logger.info('jwt has been created')
+
+    elif auth_type == "keycloak_login":
+        logger.info('auth type is keycloak_login, perform authentication using keycloak')
+
+        keycloak_host = preset.get("keycloak_host")
+        keycloak_realm = preset.get("keycloak_realm")
+        keycloak_client_id = preset.get("keycloak_client_id")
+        keycloak_client_secret = preset.get("keycloak_client_secret")
+        keycloak_username = preset.get("keycloak_username")
+        keycloak_password = preset.get("keycloak_password")
+        workspace_id = preset.get("workspace_id")
+
+        authentication_details = kdp_conn.create_keycloak_authentication_token(username=keycloak_username,
+                                                                       realm=keycloak_realm,
+                                                                       client_id=keycloak_client_id,
+                                                                       client_secret=keycloak_client_secret,
+                                                                       password=keycloak_password,
+                                                                       workspace_id=workspace_id,
+                                                                       host=keycloak_host,
+                                                                       verify_ssl=False)
+        jwt = authentication_details.get("access_token")
+        
+        logger.info('jwt has been created via keycloak')
+
     else:
         if jwt is None or jwt == "":
             raise ValueError("Please provide KDP JSON Web Token in the preset")

50b34a4c85fa59c02b1f56f9a595ed83f8e500cc
chrisdesilvakoverse <chrisdesilva@koverse>
2023-10-31 17:01:44 -0700

    code changes

--- python-lib/kdp_connector/configuration/keycloak_authentication.py
+++ python-lib/kdp_connector/configuration/keycloak_authentication.py
@@ -1,4 +1,4 @@
-from keycloak import KeycloakOpenID
+from requests import post
 
 class KeycloakAuthentication():
 
@@ -22,19 +22,41 @@ class KeycloakAuthentication():
 
         self.username = username
         self.password = password
+        
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.host = host
+        self.realm = realm
 
+        self.verify_ssl = verify_ssl
 
-        url = f"https://{host}/"
+        # url = f"https://{host}/"
 
         # Configure client - keycloak python client defaults to grant_type password and scope openid
-        self.keycloak_openid = KeycloakOpenID(server_url=url,
-                                 client_id=client_id,
-                                 realm_name=realm,
-                                 client_secret_key=client_secret)
+        # self.keycloak_openid = KeycloakOpenID(server_url=url,
+        #                          client_id=client_id,
+        #                          realm_name=realm,
+        #                          client_secret_key=client_secret)
 
-        self.keycloak_openid.connection.verify = verify_ssl
+        # self.keycloak_openid.connection.verify = verify_ssl
+
 
     def get_keycloak_token(self) -> str:
-        return self.keycloak_openid.token(username=self.username, password=self.password)['access_token']
+        request_headers = {
+         'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': '*/*',
+            'Connection': 'keep-alive',
+            'Accept-Encoding': 'gzip, deflate, br'
+        }
 
+        request_body = {
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'username': self.username,
+            'password': self.password,
+            'grant_type': 'client_credentials',
+            'scope': 'openid'
+        }
 
+        return 'Bearer ' + post(f'https://{self.host}/realms/{self.realm}/protocol/openid-connect/token', headers=request_headers, data=request_body, verify=self.verify_ssl).json()['access_token']
+        # return self.keycloak_openid.token(username=self.username, password=self.password)['access_token']
